e35543dfdaf24eeefeda9346ecd59cb4d9ffeef3
Falaxdb <Falaxdb@proton.me>
2023-12-14 01:59:28 +0100

    Rename old cat class + first implementation

--- src/main/java/ohm/softa/a03/DigestingState.java
+++ src/main/java/ohm/softa/a03/DigestingState.java
@@ -1,4 +1,8 @@
 package ohm.softa.a03;
 
-public class DigestingState {
+public class DigestingState extends State{
+    @Override
+    State successor(Cat_old cat) {
+        if ()
+    }
 }
--- src/main/java/ohm/softa/a03/State.java
+++ src/main/java/ohm/softa/a03/State.java
@@ -1,5 +1,28 @@
 package ohm.softa.a03;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public abstract class State {
+
+    protected Logger logger = LogManager.getLogger();
+
+    private int t;
+
+    private final int duration = 0;
+
+    final State tick(Cat_old cat){
+        return null;
+    }
+
+    abstract State successor(Cat_old cat);
+
+    public int getTime(){
+        return t;
+    }
+
+    public int getDuration(){
+        return duration;
+    }
 
 }
